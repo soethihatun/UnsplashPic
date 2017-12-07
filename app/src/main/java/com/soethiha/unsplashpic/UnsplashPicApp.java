0976402460d4c53a062bdd6e4a3e7c098157321b
soethiha <ygn.soethiha94@gmail.com>
2017-12-08 00:47:42 +0630

    removed kotlin file from develop branch

--- app/src/main/java/com/soethiha/unsplashpic/UnsplashPicApp.java
+++ app/src/main/java/com/soethiha/unsplashpic/UnsplashPicApp.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.widget.Toast;
 
 import com.soethiha.unsplashpic.data.models.PhotoModel;
-import com.soethiha.unsplashpic.utils.NetworkUtils;
+import com.soethiha.unsplashpic.utils.NetworkUtility;
 
 /**
  * UnsplashPic
@@ -21,11 +21,7 @@ public class UnsplashPicApp extends Application {
     public void onCreate() {
         super.onCreate();
 
-        // TODO: 12/6/17 Test
-        Toast.makeText(this, "a = " + NetworkUtils.Companion.hello(), Toast.LENGTH_SHORT).show();
-
-        if (NetworkUtils.Companion.isOnline(getApplicationContext())) {
-            Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+        if (NetworkUtility.isOnline(getApplicationContext())) {
             PhotoModel.getObjInstance().loadPhotos(getApplicationContext());
         } else {
             Toast.makeText(this, "Connection fail.", Toast.LENGTH_SHORT).show();

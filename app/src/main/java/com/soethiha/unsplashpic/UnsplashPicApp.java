88793e0aec4ae8e93b4a18b19ba59069e71ad1fa
soethiha <ygn.soethiha94@gmail.com>
2017-12-07 23:09:18 +0630

    change a file to kotlin

--- app/src/main/java/com/soethiha/unsplashpic/UnsplashPicApp.java
+++ app/src/main/java/com/soethiha/unsplashpic/UnsplashPicApp.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.widget.Toast;
 
 import com.soethiha.unsplashpic.data.models.PhotoModel;
-import com.soethiha.unsplashpic.utils.NetworkUtility;
+import com.soethiha.unsplashpic.utils.NetworkUtils;
 
 /**
  * UnsplashPic
@@ -21,7 +21,11 @@ public class UnsplashPicApp extends Application {
     public void onCreate() {
         super.onCreate();
 
-        if (NetworkUtility.isOnline(getApplicationContext())) {
+        // TODO: 12/6/17 Test
+        Toast.makeText(this, "a = " + NetworkUtils.Companion.hello(), Toast.LENGTH_SHORT).show();
+
+        if (NetworkUtils.Companion.isOnline(getApplicationContext())) {
+            Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
             PhotoModel.getObjInstance().loadPhotos(getApplicationContext());
         } else {
             Toast.makeText(this, "Connection fail.", Toast.LENGTH_SHORT).show();
